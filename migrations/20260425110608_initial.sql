7ac377d959982ec0f9bb85e2d58b1512b5ff05fd
Levio-Z <67247011+Levio-z@users.noreply.github.com>
2026-04-30 22:07:47 +0800

    feat: add triggers for chat/message

--- migrations/20260425110608_initial.sql
+++ migrations/20260425110608_initial.sql
@@ -5,6 +5,7 @@
 -- =========================
 CREATE TABLE IF NOT EXISTS users (
     id BIGSERIAL PRIMARY KEY,
+    ws_id bigint NOT NULL ,
     fullname VARCHAR(64) NOT NULL,
     email VARCHAR(64) NOT NULL,
     -- hashed argon2 password, length 97
@@ -12,7 +13,25 @@ CREATE TABLE IF NOT EXISTS users (
     created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );
 
--- 唯一索引：email
+-- workspace for users
+CREATE TABLE IF NOT EXISTS workspaces(
+  id bigserial PRIMARY KEY,
+  name varchar(32) NOT NULL UNIQUE,
+  owner_id bigint NOT NULL REFERENCES users(id),
+  created_at timestamptz DEFAULT CURRENT_TIMESTAMP
+);
+
+BEGIN;
+INSERT INTO users(id, ws_id, fullname, email, password_hash)
+  VALUES (0, 0, 'super user', 'super@none.org', '');
+INSERT INTO workspaces(id, name, owner_id)
+  VALUES (0, 'none', 0);
+COMMIT;
+-- add foreign key constraint for ws_id for users
+ALTER TABLE users
+  ADD CONSTRAINT users_ws_id_fk FOREIGN KEY (ws_id) REFERENCES workspaces(id);
+
+-- unique index on email
 CREATE UNIQUE INDEX IF NOT EXISTS email_index
 ON users(email);
 
@@ -35,6 +54,7 @@ CREATE TABLE IF NOT EXISTS chats (
     id BIGSERIAL PRIMARY KEY,
     name VARCHAR(64),
     type chat_type NOT NULL,
+    ws_id bigint NOT NULL REFERENCES workspaces(id),
     -- user id list
     members BIGINT[] NOT NULL,
     created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
